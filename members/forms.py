3957599a30d5423eeb34484eb81065c9a90a3334
Your NameNezhat <nnour002@fiu.edu>
2022-03-14 13:23:12 -0400

    added header css

--- members/forms.py
+++ members/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django import forms
 
 
+
 class RegisterUserForm(UserCreationForm):
 	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
 	first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
