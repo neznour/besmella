1a50027ad7bc247cbc38a483b95e3d0efce3c0ef
nazinourollahi <nazinourollahi@gmail.com>
2022-04-01 09:43:50 -0400

    Enabled CI

--- events/models.py
+++ events/models.py
@@ -18,7 +18,6 @@ class Venue(models.Model):
     def __str__(self):
         return self.name
 
-
 class MyUser(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
